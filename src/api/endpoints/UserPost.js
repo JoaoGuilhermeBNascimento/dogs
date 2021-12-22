d3f7c27859fd5a6b6d54ae7afb31bc68eaeca586
João Guilherme B. do Nascimento <joaoguilhermebn@hotmail.com>
2021-12-21 21:20:23 -0300

    up

--- src/api/endpoints/UserPost.js
+++ src/api/endpoints/UserPost.js
@@ -1,24 +1,36 @@
 import React from "react";
 
-
 const UserPost = () => {
-    const [username, setUsername] = React.useState('')
-    const [email, setEmail] = React.useState('')
-    const [password, setPassword] = React.useState('')
+  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-    function handleSubmit(event) {
-        event.preventDefault();
-    }
-    return 
-    <form onSubmit={handleSubmit}> 
-        <input type="text" value={username} placeholder="username" onChange={({target}) => setUsername(target.value)} />
-        <input type="text" value={email} placeholder="email" onChange={({target}) => setEmail(target.value)} />
-        <input type="text" value={password} placeholder="password" onChange={({ target }) => setPassword(target.value)} />
-        
-        <button>Enviar</button>
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  return;
+  <form onSubmit={handleSubmit}>
+    <input
+      type="text"
+      value={username}
+      placeholder="username"
+      onChange={({ target }) => setUsername(target.value)}
+    />
+    <input
+      type="text"
+      value={email}
+      placeholder="email"
+      onChange={({ target }) => setEmail(target.value)}
+    />
+    <input
+      type="text"
+      value={password}
+      placeholder="password"
+      onChange={({ target }) => setPassword(target.value)}
+    />
 
+    <button>Enviar</button>
+  </form>;
+};
 
-    </form>
-}  
-
-export default UserPost
+export default UserPost;
